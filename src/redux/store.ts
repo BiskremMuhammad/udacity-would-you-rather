14015488d0f9731c6cf55e8df520868ec4839570
Muhammad Omran <use.yurMind@gmail.com>
2021-06-06 17:07:04 +0200

    add reduc thunk

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,4 +1,5 @@
-import { createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
+import thunk from "redux-thunk";
 import { pollsReducer } from "./reducers/polls-reducer";
 
-export const store = createStore(pollsReducer);
+export const store = createStore(pollsReducer, applyMiddleware(thunk));
